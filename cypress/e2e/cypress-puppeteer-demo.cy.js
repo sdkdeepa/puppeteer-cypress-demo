052d6024a20f71ad4d9f691794aa64510cc1c4d7
Deepa Subramanian <deepa94086@gmail.com>
2024-05-17 21:29:27 -0700

    Update cypress-puppeteer-demo.cy.js

--- cypress/e2e/cypress-puppeteer-demo.cy.js
+++ cypress/e2e/cypress-puppeteer-demo.cy.js
@@ -1,5 +1,5 @@
 describe('@cypress/puppeteer demo', () => {
-  it('goes from one tab to another then comes back for assertion', () => {
+  it('Goes from one tab to another then comes back for assertion', () => {
     cy.visit('https://dev.to/sdkdeepa')
 
     cy.contains('a', 'https://deepasubramanian.dev/').click()
